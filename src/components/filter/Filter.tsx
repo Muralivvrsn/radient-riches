837d0a3fee22e9dcb365e078f49967509fe128bd
Murali <muralivvrsn75683@gmail.com>
2024-02-25 17:47:22 +0530

    responsive for filter

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -6,20 +6,20 @@ import dropdownIcon from "../../assets/dropdown.svg";
 
 export default function Filter() {
   return (
-    <div className="w-full border-t-[2px] mt-[16px] py-[14px] border-b-[2px] flex flex-wrap gap-4 sm:gap-[20px] text-[14px] text-[#4B5666] justify-between">
-      <div className="flex gap-2 sm:gap-[8px] items-center">
-        <Image src={doneIcon} alt="Done icon" width={20} height={20} />
-        <div>Last Updated</div>
-        <div>-</div>
+    <div className="w-full border-t-[2px] mt-[16px] py-2 sm:py-[14px] border-b-[2px] flex flex-wrap gap-2 sm:gap-[20px] text-xs sm:text-[14px] text-[#4B5666] justify-between">
+      <div className="flex gap-1 sm:gap-[8px] items-center">
+        <Image src={doneIcon} alt="Done icon" width={16} height={16}  />
+        <div className="hidden sm:block">Last Updated</div>
+        <div className="hidden sm:block">-</div>
         <div>February 22, 2020</div>
       </div>
-      <div className="flex gap-2 sm:gap-[8px] items-center">
-        <Image src={noteIcon} alt="Note icon" width={20} height={20} />
+      <div className="flex gap-1 sm:gap-[8px] items-center">
+        <Image src={noteIcon} alt="Note icon" width={16} height={16}  />
         <div>Advertising Disclosure</div>
       </div>
-      <div className="flex gap-2 sm:gap-[8px] items-center cursor-pointer">
+      <div className="flex gap-1 sm:gap-[8px] items-center cursor-pointer">
         <div>Top Relevant</div>
-        <Image src={dropdownIcon} alt="Dropdown icon" width={20} height={20} />
+        <Image src={dropdownIcon} alt="Dropdown icon" width={16} height={16}  />
       </div>
     </div>
   );
